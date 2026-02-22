96d359cb735cff155522f3ab1442abbc3133f0e9
Mahadia Kihame <makihame@gmail.com>
2026-02-22 22:11:21 +0000

    Initial commit: Add DNA to RNA transcription script

---
+++ transcribing_dna_into_rna.py
@@ -0,0 +1,7 @@
+def transcribe(dna_sequence):
+    #Transcies DNA string into RNA by replacing all occurrences of T with U
+    return dna_sequence.replace('T', 'U')
+
+sample_dna = 'GATGGAACTTGACTACGTAAATT'
+new_rna = transcribe(sample_dna)
+print(new_rna)
